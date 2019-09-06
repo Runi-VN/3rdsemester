dacadc483f8167bf8580946902ddd5ef9bd1b7eb
Rúni Vedel Niclasen <runiniclasen@hotmail.com>
2019-09-06 23:46:03 +0200

    fixed table for assignment
    CODE IS MESSY, TO REMIND YOU OF MY HARD WORK

--- Week36/05-thursday/DOMManipulation/src/main/webapp/js/mappingfilter.js
+++ Week36/05-thursday/DOMManipulation/src/main/webapp/js/mappingfilter.js
@@ -54,17 +54,21 @@ var cars = [
 // 1)
 // MAKES A TABLE HEADER. HAND IT AN ARRAY OF VALUES FOR A TABLE HEADER ROW.
 function tableHeader(array) {
-    let returnString = "<table class=\"table\"><thead><tr>";
+    let returnString = "<table class=\"table\"><thead>";
     array.forEach(element => returnString += "<th scope=\"col\">" + element + "</th>");
-    return returnString + "</tr></thead><tbody>";
+    return returnString + "</thead><tbody>";
 }
 
 // 2)
 // MAKES A SINGLE TABLE ROW. HAND IT AN ARRAY OF VALUES FOR A TABLE BODY ROW.
-function tableRow(array) {
+function tableRow(element) {
     let returnString = "<tr>";
     //Object.values(array).forEach(element => returnString += "<td>" + element + "</td>");
-    array.forEach(element => returnString += "<td>" + Object.values(element) + "</td>");
+    //array.forEach(element => returnString += "<td>" + Object.values(element) + "</td>");
+    console.log(element);
+    Object.values(element).forEach(element => returnString += "<td>" + element + "</td>");
+    //element.forEach(element => returnString += "<td>" + element + "</td>");
+    //array.forEach(element => returnString += "<td>" + Object.values(array[element]) + "</td>");
     return returnString + "</tr>";
 }
 
@@ -77,12 +81,21 @@ function endOfTable() {
 // END OF TABLE HELPER FUNCTIONS
 
 function generateTable(array) {
-    //Object.keys(cars[0]), Object.values(cars)
     let htmlTableString = tableHeader(Object.keys(array[0]));
-    //array.forEach(element => htmlTableString += tableRow(element));
-    htmlTableString += tableRow(array);
+
+    array.forEach(element => htmlTableString += tableRow(element));
+
     htmlTableString += endOfTable();
+
     return htmlTableString;
+//    //Object.keys(cars[0]), Object.values(cars)
+//    let htmlTableString = tableHeader(Object.keys(array[0]));
+//    //array.forEach(element => htmlTableString += tableRow(element));
+//    for (var elem in array) {
+//        htmlTableString += tableRow(array);
+//    }
+//    htmlTableString += endOfTable();
+//    return htmlTableString;
 }
 document.getElementById("insertTable").innerHTML =
         generateTable(cars);
@@ -91,5 +104,6 @@ document.getElementById("btnPrice").addEventListener("click", function (event) {
     //event.preventDefault();
     let price = document.getElementById("price").value;
     let cheapCars = cars.filter(car => car.price < price);
+    document.getElementById("insertTable").innerHTML =
     generateTable(cheapCars);
 });
