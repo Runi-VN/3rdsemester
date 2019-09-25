fe7f4b4bc8b4f0582fe49f20e914bb361d9526e9
Rúni VN <Runiniclasen@hotmail.com>
2019-09-25 22:04:13 +0200

    push bubble, update gitignore

--- Week39/25-wednesday/FetchAndPromises/src/index.js
+++ Week39/25-wednesday/FetchAndPromises/src/index.js
@@ -1,20 +1,64 @@
 import 'bootstrap/dist/css/bootstrap.css'
-import jokes from "./jokes";
+//import jokes from "./jokes";
 
-const allJokes = jokes.getJokes().map(joke => "<li>"+joke+"</li>");
-document.getElementById("jokes").innerHTML = allJokes.join("");
+/* new way */
+window.onload = function () {
+    let jokeID;
+    let joke;
+    let reference;
+    let returnString;
+
+    setInterval(function fetchJoke() {
+        fetch('https://studypoints.info/jokes/api/jokes/random') //notice changed url
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                //assumes joke object is consistent
+                jokeID = "<li> ID: " + data.id + "</li>";
+                joke = "<li> JOKE: " + data.joke + "</li>";
+                reference = "<li> REFERENCE: " + data.reference + "</li>";
+                returnString = "<ul>" + jokeID + joke + reference + "</ul>";
+            });
+
+        return fetchJoke;
+    }(), 3600000); //self-invocate once, then run every hour
 
 
-//Task 1 - Finding individual jokes
-document.getElementById("btnGetJoke").addEventListener("click", function(){
-    let id = document.getElementById("jokeID").value;
-    const getJoke = jokes.getJokeById(id);
-    document.getElementById("jokeOutput").innerHTML = getJoke;
+    document.getElementById("btnJoke").addEventListener("click", function () {
+        document.getElementById("myDiv").innerHTML = returnString;
+    })
+}
+
+/*Add the necessary event handlers, so when pressing each of the “hearts”, 
+    it will write the  message North, South, East or West respectively.*/
+document.getElementById("svgContainer").addEventListener("click", function (event) {
+    document.getElementById("svgOutput").innerHTML = event.target.parentElement.id;
+    /*
+    This is done by bubbling, to challenge myself.
+    For any element within my _svgContainer_ I add an EventListener.
+    Normally event.target equals the actual element ID (of the <RECT>-tag), such as "rect2816".
+    However I need to grab the ID of the <g>-tag, which is the parent element. (NOT parent node)
+    Only issue with this solution is that it also grabs the div-name if you click whitespace.
+    */
+
 });
 
-//Task 2 - Adding new jokes
-document.getElementById("btnAddJoke").addEventListener("click", function(){
-    let joke = document.getElementById("jokeInput").value;
-    const addJoke = jokes.addJoke(joke);
-    document.getElementById("newJokeOutput").innerHTML = "Din jokes ID er: " + addJoke + " (husk minus 1 pga. 0-indekseret)";
-});
+
+
+/* old way */
+
+// document.getElementById("btnJoke").addEventListener("click", function () {
+//     fetch('https://studypoints.info/jokes/api/jokes/random')
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             //assumes joke object is consistent
+//             let jokeID = "<li> ID:" + data.id + "</li>";
+//             let joke = "<li> JOKE: " + data.joke + "</li>";
+//             let reference = "<li> REFERENCE: " + data.reference + "</li>";
+//             let returnString = "<ul>" + jokeID + joke + reference + "</ul>";
+//             document.getElementById("myDiv").innerHTML = returnString;
+//         });
+// })
